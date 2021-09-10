82e065a6882e707546021b140874b6702a1953a4
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-10 12:04:20 +0500

    style props types

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Person from './components/Person';
 import Greet from './components/Greet';
 import './index.css';
 import PersonList from './components/PersonList';
+import Container from './components/Container';
 
 function App() {
   const personname={
@@ -28,7 +29,7 @@ function App() {
   <Greet name="Nauman" message={12} isLoggedIn={false} />
   <Person name={personname}/>  
   <PersonList names={nameList}/>
-  
+  <Container styles={{border:'1px solid gray'}} />
    </div>
   );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+type ContainerProps={
+    styles:React.CSSProperties
+}
+const Container = (props:ContainerProps) => {
+    return (
+        <div style={props.styles}>
+            Text contain here
+        </div>
+    )
+}
+
+export default Container
