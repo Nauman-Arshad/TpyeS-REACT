bbc7ae32c2c4dd7285e83b293a491e23b55e5a73
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-12 20:46:24 +0500

    usestate vlaues type

---
+++ src/components/state/User.tsx
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+
+type AuthUser = {
+    name: string
+    email: string
+}
+
+export const User = () =>{
+    const [user, setUser] = useState<null | AuthUser>(null)
+    const handleLogin = () => {
+        setUser({
+            name: 'Nauman',
+            email: 'nauman@exp.com'
+        })
+    }
+    const handleLogout=()=>{
+        setUser(null)
+    }
+
+    return(
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+             <div>User Name is {user?.name}</div>
+             <div>User Name is {user?.email}</div>
+
+        </div>
+    )
+}
